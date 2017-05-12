b7357984c04de805418402bf227c03756ffbce0b
Marina <maggalka@mail.ru>
2017-05-12 16:24:05 +0300

    Preparing for HW5. Profiles, connection pool, SpringDataJPA, Spring cache.

---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Meal;
+
+public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+}
