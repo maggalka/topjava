ee63cb347c7587da95926d1434b8b3adf3df6441
Marina <maggalka@mail.ru>
2017-05-20 00:50:04 +0300

    HW5 from Grigory, Profiles. Question.

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -17,5 +17,5 @@ public class Profiles {
             JPA = "jpa",
             DATAJPA = "datajpa";
 
-    public static final String REPOSITORY_IMPLEMENTATION = JPA;
+    public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
 }
